95d838150081097c4e20f8793bdaf460571034ca
Christos Avrilionis <avrilionis@gmail.com>
2025-03-11 22:08:39 +0100

    Removed gm, added suppressPackageStartupMessages()

--- global.R
+++ global.R
@@ -1,5 +1,4 @@
-library(shiny)
-suppressPackageStartupMessages(library(gm))
+suppressPackageStartupMessages(library(shiny))
 library(shinythemes)
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(shinyjs))
