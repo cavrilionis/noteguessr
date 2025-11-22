fdf01fbe0c8c238df82afca96e0d41994fa79981
Christos Avrilionis <avrilionis@gmail.com>
2025-11-22 22:01:29 +0100

    First div align-items center. Second div margin-left 20 px.

--- ui.R
+++ ui.R
@@ -23,13 +23,13 @@ fluidPage(
     ),
     mainPanel(
       tags$div(
-        style = "display: flex; flex-direction: column; align-items: left;",
+        style = "display: flex; flex-direction: line; align-items: center;",
         tags$div(
-          style = "max-height: 150px; overflow: hidden;", # Adjust max-height as needed
+          style = "max-height: 150px; overflow: hidden;",
           imageOutput("noteImage")
         ),
         tags$div(
-          style = "margin-top: 5px; margin-bottom: 0px;",
+          style = "margin-top: 0px; margin-bottom: 0px; margin-left: 20 px;",
           textOutput("result"),
           textOutput("percentage")
         )
